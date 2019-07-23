9d488597f571ae5c19e81905709d0b0f81e04dd5
zpallday <zpman95@hotmail.com>
2019-07-23 17:50:04 -0500

    JS, branch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -9,7 +9,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
+   
   // Potential Solution:
 
   // Higher order function using "cb" as the call back
@@ -27,25 +27,52 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, function(arrLength){
+  console.log(arrLength)
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(lastitem) {
+  console.log(lastitem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y);
 }
+sumNums(5, 9, function(x,y) {
+  console.log(x + y);
+});
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y);
 }
+sumNums(2, 6, function(x,y) {
+  console.log(x * y);
+});
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
-
+contains('Pencil', items, function(callback){
+  console.log(callback)
+})
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
